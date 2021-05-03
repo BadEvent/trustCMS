81fc6ad5667818f278be0cac8dc89b479a3cb0cd
badevent <Makarovee98@gmail.com>
2021-05-03 20:16:20 +0700

    fix: add main seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,12 +7,17 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            RoleIdSeeder::class,
+            OrganizationSeeder::class,
+            DataSeeder::class,
+            UserSeeder::class,
+        ]);
     }
 }
